b9b57f5c51e7f3945838ce68b8ed891119898cfa
Alec Scott <alecbcs@gmail.com>
2019-04-03 18:52:44 -0700

    Initial Commit!

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/alecbcs/lookout
+
+go 1.12
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/DataDrake/cuppa v1.0.4
+)
---
+++ lookout.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DataDrake/cuppa/providers"
+	"github.com/DataDrake/cuppa/results"
+)
+
+// CheckUpdate checks a given URL for the latest available release.
+func CheckUpdate(archive string) {
+
+	for _, p := range providers.All() {
+		name := p.Match(archive)
+		if name == "" {
+			continue
+		}
+		r, s := p.Latest(name)
+		if s != results.OK {
+			continue
+		}
+		loc := r.Location
+		ver := r.Version
+		fmt.Println(loc)
+		fmt.Println(ver)
+
+	}
+}
+
+func main() {
+	var archive string
+	fmt.Scan(&archive)
+	CheckUpdate(archive)
+}
