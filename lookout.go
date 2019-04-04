d39eb06f7fe057bfd1dd80e13bf3b10f38d00246
Alec Scott <alecbcs@gmail.com>
2019-04-04 12:23:33 -0700

    Initial Commit of Modular Design

--- lookout.go
+++ lookout.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/DataDrake/cuppa/providers"
-	"github.com/DataDrake/cuppa/results"
+	"github.com/alecbcs/lookout/update"
 )
 
-// CheckUpdate checks a given URL for the latest available release.
-func CheckUpdate(archive string) {
-
-	for _, p := range providers.All() {
-		name := p.Match(archive)
-		if name == "" {
-			continue
-		}
-		r, s := p.Latest(name)
-		if s != results.OK {
-			continue
-		}
-		loc := r.Location
-		ver := r.Version
-		fmt.Println(loc)
-		fmt.Println(ver)
-
-	}
-}
-
 func main() {
-	var archive string
-	fmt.Scan(&archive)
-	CheckUpdate(archive)
+	update.CheckUpdate("https://github.com/DataDrake/cuppa/archive/v1.0.4.tar.gz")
 }
