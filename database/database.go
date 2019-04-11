2de217f5bfc8a893cffd3853fe80cb54315775f1
Alec Scott <alecbcs@gmail.com>
2019-04-11 13:48:26 -0700

    Initial implementation of database.

--- database/database.go
+++ database/database.go
@@ -1,6 +1,39 @@
 package database
 
 import (
-	"github.com/mattn/go-sqlite3"
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS appdata(id TEXT, url TEXT, version TEXT);")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Add inserts a new entry into the database.
+func Add(id string, url string, version string) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO appdata(id, url, version) VALUES(?,?,?);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(id, url, version)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- lookout.go
+++ lookout.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/alecbcs/lookout/database"
 	"github.com/alecbcs/lookout/update"
 )
 
@@ -10,4 +11,5 @@ func main() {
 	source := "https://github.com/DataDrake/cuppa/archive/v1.0.4.tar.gz"
 	version, location := update.CheckUpdate(source)
 	fmt.Println(version, location)
+	database.Add("cuppa", location, version)
 }
