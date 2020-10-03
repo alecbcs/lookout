20f2a8700717739e3f4994f79ec745816c5b0db8
vsoch <vsochat@stanford.edu>
2020-10-03 13:25:14 -0600

    moving check for token during check for updates
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- update/update.go
+++ update/update.go
@@ -11,11 +11,7 @@ import (
 
 func init() {
 	// Port Lookout configuration to CUPPA
-        cuppa.Global.Github.Key = config.Global.Github.Key
-
-	if config.Global.Github.Key == "" {
-		log.Fatal("A GitHub token is required in your $HOME/.config/lookout/lookout.config")
-	}
+	cuppa.Global.Github.Key = config.Global.Github.Key
 }
 
 // CheckUpdate checks a given URL for the latest available release.
@@ -30,9 +26,13 @@ func CheckUpdate(archive string) (*results.Result, bool) {
 			continue
 		}
 
+		// GitHub will never work without a token
+		if p.Name() == "GitHub" && cuppa.Global.Github.Key == "" {
+			log.Fatal("A GitHub token is required in your $HOME/.config/lookout/lookout.config")
+		}
+
 		// Pull the latest (non-beta) release from repository.
 		r, s := p.Latest(name)
-
 		if s != results.OK {
 			continue
 		}
